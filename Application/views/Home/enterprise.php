40dff1b5e257e5eaa6f9a15a45c681dcbe6d8444
Leticia <lmossicardoso@gmail.com>
2024-03-25 21:29:17 -0300

    adicionando HTML da Home para empresa

---
+++ Application/views/Home/enterprise.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BussFeed | Home</title>
+</head>
+
+<body>
+    <header>
+
+    </header>
+    <main>
+        <section>
+            <div>
+                <p>Hoje, 15 de maio</p>
+                <p>Estimativa: 20 pessoas</p>
+            </div>
+            <a href="#">
+                <div>
+                    <!--Botão-->
+                    <p>Viagem de Hoje</p>
+                    <p>45</p>
+                </div>
+            </a>
+        </section>
+        <section>
+            <h3>Cadastros e alterações</h3>
+            <a href="#">
+                <div>
+                    <!--Botão-->
+                    <p>Veículo</p>
+                </div>
+            </a>
+            <a href="#">
+                <div>
+                    <!--Botão-->
+                    <p>Motorista</p>
+                </div>
+            </a>
+            <a href="#">
+                <div>
+                    <!--Botão-->
+                    <p>Pontos</p>
+                </div>
+            </a>
+            <a href="#">
+                <div>
+                    <!--Botão-->
+                    <p>Minhas viagens</p>
+                </div>
+            </a>
+        </section>
+    </main>
+</body>
+
+</html>
