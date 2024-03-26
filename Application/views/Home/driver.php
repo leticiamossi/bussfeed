514c689ba1f1668a0fbd20ae2e80c56115f274b1
Leticia <lmossicardoso@gmail.com>
2024-03-25 21:34:49 -0300

    Adicionando HTML da Home para motoristas

---
+++ Application/views/Home/driver.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BussFeed | Home</title>
+</head>
+
+<body>
+    <header>
+
+    </header>
+    <main>
+        <section>
+            <p>Hoje, 15 de maio</p>
+            <p>Veiculo: xxx</p>
+            <p>Quant. passageiros: xx</p>
+            <p>Destino: xxx</p>
+        </section>
+        <section>
+            <a href="#">
+                <div>
+                    <!--Botão-->
+                    <p>Minhas viagens</p>
+                </div>
+            </a>
+        </section>
+    </main>
+</body>
+
+</html>
