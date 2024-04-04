afedac204ae15e701bd4eb51476a0b865da74f1f
Leticia <lmossicardoso@gmail.com>
2024-04-04 19:09:12 -0300

    Adicionando paginas de consulta

---
+++ Application/views/consultas/vehicle.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BussFeed | Veiculos</title>
+</head>
+<body>
+    <header></header>
+    <main>
+        <h3>Meus Veículos</h3>
+        <div>
+            <div>
+                <h4>Veiculo 1</h4>
+                <p>Status: Ativo</p>
+                <img src="" alt="">
+            </div>
+            <div>
+                <div>
+                    <p>Modelo</p>
+                    <p>Van</p>
+                </div>
+                <div>
+                    <p>Placa</p>
+                    <p>XXXX XXXX</p>
+                </div>
+                <div>
+                    <p>Marca</p>
+                    <p>Mercedes</p>
+                </div>
+                <div>
+                    <p>Quantidade de lugares</p>
+                    <p>12</p>
+                </div>
+                <div>
+                    <p>Situação</p>
+                    <p>Próprio</p>
+                </div>
+            </div>
+        </div>
+        <button>Cadastrar novo veículo</button>
+    </main>
+</body>
+</html>
