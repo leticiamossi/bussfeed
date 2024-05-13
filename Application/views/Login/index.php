1649d9abda9ea1b306ec943bd9fa3c8551103a55
Eldritchunter <eldritchunter@users.noreply.github.com>
2024-05-13 20:01:00 -0300

    Início da nomeação das classes para botões gerais.

--- Application/views/Login/index.php
+++ Application/views/Login/index.php
@@ -5,20 +5,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BussFeed | Login</title>
+    <link rel="stylesheet" href="public\assets\css\style.css">
 </head>
 <body>
-    <form action="" method="POST">
-        <div>
-            <label for="email">E-mail</label>
-            <input type="text" name="email">
-        </div>
-        <div>
-            <label for="senha">Senha</label>
-            <input type="text" name="senha">
-        </div>
-        <input type="submit" value="Entrar">
-    </form>
-    <div>
+    <div class="login">
+        <form action="" method="POST">
+            <div>
+                <label for="email">E-mail</label>
+                <input class = email type="text" name="email">
+            </div>
+            <div>
+                <label for="senha">Senha</label>
+                <input class = senha type="text" name="senha">
+            </div>
+            <input class = entrar type="submit" value="Entrar">
+        </form>
+    </div>
+    <div class="container">
         <p>ou</p>
         <a href="#">Cadastre-se aqui</a>
     </div>
