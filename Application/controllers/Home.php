d408a9e1a1989a8231a3a58705fc660516ba5bd5
Leticia <lmossicardoso@gmail.com>
2024-06-16 15:44:17 -0300

    Aparecer para passageiro apenas viagens ainda não confirmadas

--- Application/controllers/Home.php
+++ Application/controllers/Home.php
@@ -23,7 +23,21 @@ class Home extends Controller
         if($this->permission){
             $id = $_SESSION['ID_EMP'];
             $conn = $this->model('home');
-            $viagens = $conn::listViagens($id);
+            $viagens_aux = $conn::listViagens($id);
+            $confirm = $conn::listViagensConfirm($_SESSION['ID']);
+
+            $viagens = [];
+            foreach($viagens_aux as $viagem){
+                $confirmado = false;
+                foreach($confirm as $id){
+                    if($viagem['id_viagem'] == $id['idviagem']){
+                        $confirmado = true;
+                    }
+                }
+                if(!$confirmado){
+                    array_push($viagens, $viagem);
+                }
+            }
             $this->view('home/passenger', ['viagens' => $viagens]);
         } else {
             $this->view('erro404');
--- Application/models/home.php
+++ Application/models/home.php
@@ -13,6 +13,13 @@ class Home
         $result = $conn->executeQuery("SELECT * FROM tb_viagem WHERE dataViagem_viagem = CURDATE() && empresa_id = :ID", array(':ID' => $idEmpresa));
         return $result->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public static function listViagensConfirm(string $idAluno)
+    {
+        $conn = new Database();
+        $result = $conn->executeQuery("SELECT idviagem FROM tb_pontoesperaaluno WHERE idaluno = :ID", array(':ID' => $idAluno));
+        return $result->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
 
 ?>
