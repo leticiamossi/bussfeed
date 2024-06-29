ec5bc37be65ba47f35ea33161a6cfbb5cd58ecab
Eldritchunter <rainyfeetcomm@gmail.com>
2024-06-29 13:55:55 -0300

    Ajuste Consulta/Minhas solicitações = Passageiro.

--- Application/views/solicitacoes/index.php
+++ Application/views/solicitacoes/index.php
@@ -5,9 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BussFeed | Solicitações</title>
-    <link rel="stylesheet" href="../../../public/assets/css/style.css">
+    <link rel="stylesheet" href="../../../public/assets/css/styleConsulta.css">
 </head>
 <body>
+    <?php require './Application/views/headers/headerPas.php'?>
     <header>
         <a href="/login/logout">Sair</a>
     </header>
@@ -19,13 +20,14 @@
                 <h5><?php echo $solicitacao['nome_aluno'] ?></h5>
                 <p><?php echo $solicitacao['cpf_aluno'] ?></p>
                 <div>
-                    <a href="/solicitacoes/resposta/<?php echo $solicitacao['id_alunoEmpresa'] ?>/0"><button>Recusar</button></a>
-                    <a href="/solicitacoes/resposta/<?php echo $solicitacao['id_alunoEmpresa'] ?>/1"><button>Aceitar</button></a>
+                    <a href="/solicitacoes/resposta/<?php echo $solicitacao['id_alunoEmpresa'] ?>/0"><button class="input">Recusar</button></a>
+                    <a href="/solicitacoes/resposta/<?php echo $solicitacao['id_alunoEmpresa'] ?>/1"><button class="entrar">Aceitar</button></a>
                 </div>
             </div>
         <?php } ?>
        </div>
     </main>
+    <?php require './Application/views/headers/footerPas.php'?>
 </body>
 
 </html>
